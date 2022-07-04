c0f54edbce92c1830654b769b22cb397a012594e
= <n.wright.online@gmail.com>
2022-07-03 22:19:31 -0500

    Update profile name and occupation on submit

--- scripts/index.js
+++ scripts/index.js
@@ -25,14 +25,31 @@ let initialCards = [
   },
 ];
 
-let modal = document.querySelector(".modal");
-const editButton = document.querySelector(".profile__edit-button");
+const editProfileButton = document.querySelector(".profile__edit-button");
 const closeModalButton = document.querySelector(".modal__form-close-button");
+const modal = document.querySelector(".modal");
+const profileFormElement = document.querySelector(".modal__form");
+const nameInput = document.querySelector("#name");
+const jobInput = document.querySelector("#job");
+const profileName = document.querySelector(".profile__name");
+const profileJob = document.querySelector(".profile__occupation");
 
-editButton.addEventListener("click", () => {
-  modal.classList.add("modal__opened");
-});
+function toggleModalWindow() {
+  nameInput.value = document.querySelector(".profile__name").textContent;
+  jobInput.value = document.querySelector(".profile__occupation").textContent;
+  modal.classList.toggle("modal__opened");
+}
 
-closeModalButton.addEventListener("click", () => {
+editProfileButton.addEventListener("click", toggleModalWindow);
+closeModalButton.addEventListener("click", toggleModalWindow);
+
+function handleProfileFormSubmit(e) {
+  e.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+
   modal.classList.remove("modal__opened");
-});
+}
+
+profileFormElement.addEventListener('submit', handleProfileFormSubmit);
