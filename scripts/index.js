6b7f14925bc8c6c1dfa00f3c74d122c890ce96c2
= <n.wright.online@gmail.com>
2022-07-04 10:10:57 -0500

    Change variable and function names, clean up code

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let initialCards = [
+const initialCards = [
   {
     name: "Yosemite Valley",
     link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
@@ -26,8 +26,8 @@ let initialCards = [
 ];
 
 const editProfileButton = document.querySelector(".profile__edit-button");
-const closeModalButton = document.querySelector(".modal__form-close-button");
-const modal = document.querySelector(".modal");
+const closeProfileModalButton = document.querySelector(".modal__form-close-button");
+const profileModal = document.querySelector(".modal");
 const profileFormElement = document.querySelector(".modal__form");
 const nameInput = document.querySelector("#name");
 const jobInput = document.querySelector("#job");
@@ -36,23 +36,22 @@ const profileJob = document.querySelector(".profile__occupation");
 const cardList = document.querySelector(".elements__list");
 const cardTemplate = document.querySelector("#card-template").content;
 
+function fillProfileForm() {
+  nameInput.value = document.querySelector(".profile__name").textContent;
+  jobInput.value = document.querySelector(".profile__occupation").textContent;
+}
+
 function openProfileModal() {
-  modal.classList.add("modal__opened");
+  fillProfileForm();
+  profileModal.classList.add("modal__opened");
 }
 
 function closeProfileModal() {
-  modal.classList.remove("modal__opened");
+  profileModal.classList.remove("modal__opened");
 }
 
-// Remove this block
-function toggleModalWindow() {
-  nameInput.value = document.querySelector(".profile__name").textContent;
-  jobInput.value = document.querySelector(".profile__occupation").textContent;
-  modal.classList.toggle("modal__opened");
-}
-
-editProfileButton.addEventListener("click", toggleModalWindow);
-closeModalButton.addEventListener("click", toggleModalWindow);
+editProfileButton.addEventListener("click", openProfileModal);
+closeProfileModalButton.addEventListener("click", closeProfileModal);
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
@@ -60,19 +59,17 @@ function handleProfileFormSubmit(evt) {
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
 
-  modal.classList.remove("modal__opened");
+  closeProfileModal();
 }
 
 profileFormElement.addEventListener('submit', handleProfileFormSubmit);
 
 
-function getCardElement(data) {
+function renderCards(data) {
   initialCards.forEach(function(obj) {
     const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
     const cardTitle = cardElement.querySelector(".card__title");
     const cardImage = cardElement.querySelector(".card__photo");
-    cardImage.style.borderTopLeftRadius = "10px";
-    cardImage.style.borderTopRightRadius = "10px";
     cardImage.src = obj.link;
     cardImage.alt = obj.name;
     cardTitle.textContent = obj.name;
@@ -81,4 +78,4 @@ function getCardElement(data) {
 }
 
 
-getCardElement(initialCards);
+renderCards(initialCards);
